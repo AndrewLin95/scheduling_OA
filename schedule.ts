357ba4afbc519d910ba8fe3be243bf932ff9094c
AndrewLin95 <andrew.lin88@hotmail.com>
2023-01-22 12:17:15 -0500

    scaffold for schedule

---
+++ schedule.ts
@@ -0,0 +1,13 @@
+const single = "single";
+const double = "double";
+const commercial = "commercial";
+
+const certified = "certified";
+const pendingCert = "installer pending certification";
+const laborer = "laborer";
+
+const buildings = [single, double, commercial]
+
+const schedule = (buildings, employees) => {
+  return null;
+}
