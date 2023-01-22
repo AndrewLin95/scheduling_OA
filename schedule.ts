ca73bc0519957a2a6cf5682a1613535676bcb98d
AndrewLin95 <andrew.lin88@hotmail.com>
2023-01-22 13:52:10 -0500

    formatted schema for results

--- schedule.ts
+++ schedule.ts
@@ -1,4 +1,5 @@
 // START: 12:30 PM 22Jan2023
+// npx ts-node schedule.ts
 
 import { daysOfMonth, Employee } from "./models";
 
@@ -18,7 +19,7 @@ const schedule = (buildings: string[], employees: Record<daysOfMonth, Employee>)
   //    Within the array, return an object with string for building type, and the number / type of employee required for the build.
 
   const numberOfBuildingsCheck = (buildings: string[], employees: Employee) => {
-    let buildingsToDo: string[] = [];
+    let buildingsToDo: any = [];
 
     let i = 0;
     let countOfCertified = 0;
@@ -27,16 +28,48 @@ const schedule = (buildings: string[], employees: Record<daysOfMonth, Employee>)
         countOfCertified += 2;
         // if adding this building type exceeds the number of certified workers, do not add and remove the count then iterate.
         if (countOfCertified > employees.certified) {
-          countOfCertified -= 2;
+          // to implement: If the cut off is at commercial, check if a double is after it. Since the number of workers is less, even though commercial is cut off, can may still fulfill the double building.
+          // countOfCertified -= 2;
+          return buildingsToDo;
         } else {
-          buildingsToDo.push(buildings[i]);
+          const obj = {
+            index : i,
+          }
+          const builder = {
+            certified: 1,
+          }
+          obj[buildings[i]] = builder
+
+          buildingsToDo.push(obj);
         }
       } else {
-        countOfCertified ++;
-        buildingsToDo.push(buildings[i]);
-      }
+        countOfCertified ++; 
+        const obj = {
+          index : i,
+        }
+        const builder = {
+          certified: 1,
+        }
+        obj[buildings[i]] = builder
+
+        buildingsToDo.push(obj);
+      } 
       i++;
     };
+    // [ 'single', 'double', 'commercial', 'single' ]
+    // check remaining laborers.
+    // ideally, assign pendingCerts to commercial, then assign laborers to rest...
+    let countOfPendingCert = 0;
+    let countOfLaborer = 0;
+
+    let j = 0;
+    while (j < buildingsToDo.length) {
+      if (buildingsToDo[j] = double) {
+        
+      }
+    }
+
+
     return buildingsToDo;
   }
 
@@ -80,5 +113,34 @@ const employees = {
 const result = schedule(buildings, employees);
 
 const tempResultOutputSchema = {
-  
+  monday: [
+    {
+      index: 1,
+      single: {
+        certified: 1
+      }
+    },
+    {
+      index: 2,
+      double: {
+        certified: 1,
+        pendingCert: 1,
+      }
+    }
+  ],
+  tuesday: [
+    {
+      index: 1,
+      single: {
+        certified: 1
+      }
+    },
+    {
+      index: 2,
+      double: {
+        certified: 1,
+        pendingCert: 1,
+      }
+    }
+  ]
 }
